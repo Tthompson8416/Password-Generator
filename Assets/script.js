76f3e6a88bc3be2faa67d4be9e4c92abbe0054a3
TT84 <tthompson8416@outlook.com>
2021-12-18 23:35:57 -0500

    Added random character options to javascript

--- Assets/script.js
+++ Assets/script.js
@@ -3,17 +3,37 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
+}
+
+// created random lower case string
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+// created random upper case string
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+
+}
+  console.log(getRandomSymbol());
 
        var chars =  "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
        var passwordLength = 8;
        var password = "";
-       for (var i = 0; i <=passwordLength; i++) {
-       var randomNumber = Math.floor(Math.random() * chars.length);
+       for (var i = 0; i <=passwordLength; ++i) {
+       var randomNumber = Math.floor(Math.random() * chars.passwordLength);
        password += chars.substring(randomNumber, randomNumber +1);
       }
 
-
-    }
 
 function writePassword() {
   var password = generatePassword();
