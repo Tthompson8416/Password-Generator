c4243f34693e7c3e458dbcdbbdedfcece37929b5
TT84 <tthompson8416@outlook.com>
2021-12-19 21:31:53 -0500

    Fixed conole error in DOM elements section

--- Assets/script.js
+++ Assets/script.js
@@ -5,28 +5,25 @@ var generateBtn = document.querySelector("#generate");
 
 // Testing js code DOM elements
 const resultEl = document.getElementById("password");
-const lengthEl = document.getElementById("password");
+const lengthEl = document.getElementById("checkbox1");
 const uppercaseEl = document.getElementById("checkbox2");
 const lowercaseEl = document.getElementById("checkbox3");
 const numbersEl = document.getElementById("checkbox4");
 const symbolsEl = document.getElementById("checkbox5");
 const generateEl = document.getElementById("password");
-const clipboardEl = document.getElementById("password");
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
+console.log('Happy');
+
 
 // Generate event listen
-generateEl.addEventListener("click", () => {
+generateEl.addEventListener("checked", () => {
   const length = +lengthEl.value;
   const hasLower = lowercaseEl.checked;
   const hasUpper = uppercaseEl.checked;
   const hasNumber = numbersEl.checked;
   const hasSymbol = symbolsEl.checked;
+
+  console.log('Morning');
 
 
 resultEl.innerText = generatePassword(
@@ -40,48 +37,30 @@ resultEl.innerText = generatePassword(
 
 //Generate password function
 function generatePassword(lower, upper, number, symbol, length) {
+const chosenchars = []
+  const symbolArray = ['!', '@', '#', '$','%','^','&','*']
+  if (symbol === true)  {
+    chosenchars.push(symbolArray)
+    console.log('hello');
 
-var chars =  "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-       var passwordLength = 8;
-       var password = "";
-       for (var i = 0; i <=passwordLength; ++i) {
-       var randomNumber = Math.floor(Math.random() * chars.passwordLength);
-       password += chars.substring(randomNumber, randomNumber +1);
-       return String.randomFunc("")
-       }
+
+  }
+
+// var chars =  "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//        let passwordLength = 8;
+//        let password = "";
+//        for (var i = 0; i <=passwordLength; ++i) {
+//        let randomNumber = Math.floor(Math.random() * chars.passwordLength);
+//        password += chars.substring(randomNumber, randomNumber +1);
+//        return password;
+//        }
+
+console.log(chosenchars);
+
+return chosenchars[0]
 
 let generatePassword = "";
 }
-
-
-// random lower case string
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-// random upper case string
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-// random number string
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-// random symbol string
-function getRandomSymbol() {
-  const symbols = "!@#$%^&*";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-
-}
-  console.log(getRandomLower());
-  console.log(getRandomUpper());
-  console.log(getRandomNumber());
-  console.log(getRandomSymbol());
-
-
-
 
 function writePassword() {
   var password = generatePassword();
